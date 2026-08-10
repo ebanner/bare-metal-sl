abb184a2f3de68a8273a80560d7c767e12cbec71
Edward Banner <edward.banner@gmail.com>
2026-08-10 19:32:57 -0400

    Add endwin()

--- src/lib/curses.h
+++ src/lib/curses.h
@@ -21,14 +21,18 @@ void initscr(void);
 
 int noecho(void) { return OK; }
 
-int curs_set(int visibility) { return OK; }
+int curs_set(int) { return OK; }
 
 int getch(void) { return ERR; }
 
-int nodelay(WINDOW *win, int bf) { return OK; }
+int nodelay(WINDOW*, int) { return OK; }
 
-int leaveok(WINDOW *win, int bf) { return OK; }
+int leaveok(WINDOW*, int) { return OK; }
 
-int scrollok(WINDOW *win, int bf) { return OK; }
+int scrollok(WINDOW*, int) { return OK; }
 
 int refresh(void) { return OK; }
+
+int mvcur(int, int, int, int) { return OK; }
+
+int endwin(void) { return OK; }
